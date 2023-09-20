d75476ca530bef5d57c473d3593b96cc8ccb05ca
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2023-09-20 09:25:41 +0900

    Install react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { Navbar } from "./components/Navbar";
 import { Home } from "./pages/Home";
+import { Status } from "./pages/Status";
 
 function App() {
   return (
     <>
-      <Navbar></Navbar>
-      <Home></Home>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/status" element={<Status />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
