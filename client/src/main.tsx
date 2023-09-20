daa5626eb489aa2efc05b21854418c992c7d1b98
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2023-09-20 21:56:07 +0900

    Add html5-qrcode integration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,9 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename="/">
+    <App />
+  </BrowserRouter>
 );
