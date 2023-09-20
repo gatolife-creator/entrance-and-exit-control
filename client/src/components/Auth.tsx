bafdf8cab43df7e055aa8c6db013c7e8c1f12d14
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2023-09-20 23:36:06 +0900

    Fix small bugs

--- client/src/components/Auth.tsx
+++ client/src/components/Auth.tsx
@@ -1,47 +1,105 @@
-import { useEffect, useState } from "react";
-import Html5QrcodePlugin from "./Html5QrcodePlugin";
+import { FormEvent, useState } from "react";
+import { QrReader } from "react-qr-reader";
 
 export const Auth = () => {
+  const [uuid, setUuid] = useState("");
   const [isReaderOn, setIsReaderOn] = useState(false);
+  const [isRead, setIsRead] = useState(false);
+
   const clickHandler = () => {
     setIsReaderOn(true);
   };
-  const onNewScanResult = (decodedText: string) => {
-    console.log(decodedText);
-    (
-      document.querySelector(
-        "#html5-qrcode-button-camera-stop"
-      ) as HTMLButtonElement
-    ).click();
-    setIsReaderOn(false);
-  };
 
-  useEffect(() => {
-    if (document.querySelector("#html5-qrcode-button-camera-permission")) {
-      (
-        document.querySelector(
-          "#html5-qrcode-button-camera-permission"
-        ) as HTMLButtonElement
-      ).click();
+  const submitHandler = async (e: FormEvent) => {
+    e.preventDefault();
+    const res = await fetch("/api/auth/signinAsAdmin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        uuid,
+        password: e.currentTarget.textContent,
+      }),
+    });
+
+    if (res.status === 200) {
+      console.log("successfully logged in");
     }
-  }, [isReaderOn]);
+  };
 
   return (
     <div className="w-[300px] h-[450px] mx-auto mt-16 border shadow-xl rounded-lg">
       <div className="g-6 flex h-full flex-wrap items-center justify-center">
-        <div className="mb-12 md:mb-0 w-full text-center">
-          {isReaderOn && (
-            <Html5QrcodePlugin
-              fps={10}
-              qrbox={250}
-              disableFlip={false}
-              qrCodeSuccessCallback={onNewScanResult}
-            />
+        <div className="mb-12 md:mb-0 w-full text-center p-5">
+          {!isRead && (
+            <>
+              {isReaderOn && (
+                <QrReader
+                  onResult={async (result) => {
+                    console.log(result?.getText());
+                    if (result) {
+                      navigator.mediaDevices
+                        .getUserMedia({
+                          audio: false,
+                          video: true,
+                        })
+                        .then((stream) => {
+                          stream.getTracks().forEach(function (track) {
+                            track.stop();
+                            track.enabled = false;
+                          });
+                        });
+                      setIsRead(true);
+                      setUuid(result.getText());
+                    }
+                  }}
+                />
+              )}
+              <button className="btn btn-primary mt-5" onClick={clickHandler}>
+                Read QRCode
+              </button>
+            </>
           )}
+          {isRead && (
+            <>
+              <form onSubmit={submitHandler}>
+                <h1 className="text-2xl">Successful scanned!</h1>
+                <p className="text-lg">Please complete your password.</p>
+                <div className="relative mb-6" data-te-input-wrapper-init>
+                  <input
+                    type="text"
+                    className="input input-bordered w-full max-w-xs"
+                    id="id"
+                    placeholder="id"
+                    defaultValue={uuid}
+                    hidden
+                  />
+                </div>
 
-          <button className="btn btn-primary mt-5" onClick={clickHandler}>
-            Read QRCode
-          </button>
+                <div className="relative mb-6" data-te-input-wrapper-init>
+                  <input
+                    type="password"
+                    className="input input-bordered w-full max-w-xs"
+                    id="exampleFormControlInput22"
+                    autoComplete="true"
+                    placeholder="Password"
+                  />
+                </div>
+
+                <div className="text-center">
+                  <button
+                    type="submit"
+                    className="btn btn-success"
+                    data-te-ripple-init
+                    data-te-ripple-color="light"
+                  >
+                    Login
+                  </button>
+                </div>
+              </form>
+            </>
+          )}
         </div>
       </div>
     </div>
