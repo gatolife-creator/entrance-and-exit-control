8b752e39e195b733e7771d61bf0f5850122e6922
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2023-09-22 10:15:11 +0900

    Chore

--- client/src/components/Auth.tsx
+++ client/src/components/Auth.tsx
@@ -10,7 +10,7 @@ export const Auth = (props: Props) => {
   const [password, setPassword] = useState("");
   const [isReaderOn, setIsReaderOn] = useState(false);
   const [isRead, setIsRead] = useState(false);
-  const [isValid, setIsValid] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
     checkIfSignedIn();
@@ -34,7 +34,7 @@ export const Auth = (props: Props) => {
     });
 
     if (res.status === 200) {
-      setIsValid(true);
+      setIsAuthenticated(true);
     }
   };
 
@@ -47,93 +47,94 @@ export const Auth = (props: Props) => {
     });
 
     if (res.status === 200) {
-      setIsValid(true);
+      setIsAuthenticated(true);
     }
+  };
+
+  const PasswordForm = () => {
+    return (
+      <div className="w-[300px] h-[450px] mx-auto mt-16 border shadow-xl rounded-3xl">
+        <div className="g-6 flex h-full flex-wrap items-center justify-center">
+          <div className="mb-12 md:mb-0 w-full text-center p-5">
+            {!isRead && isReaderOn && (
+              <>
+                <QrReader
+                  onResult={async (result) => {
+                    if (result) {
+                      navigator.mediaDevices
+                        .getUserMedia({
+                          audio: false,
+                          video: true,
+                        })
+                        .then((stream) => {
+                          stream.getTracks().forEach(function (track) {
+                            track.stop();
+                            track.enabled = false;
+                          });
+                        });
+                      setIsRead(true);
+                      setUuid(result.getText());
+                    }
+                  }}
+                  constraints={{ facingMode: undefined }}
+                />
+              </>
+            )}
+            {!isRead && (
+              <button className="btn btn-primary mt-5" onClick={clickHandler}>
+                Read QRCode
+              </button>
+            )}
+            {isRead && (
+              <>
+                <form onSubmit={submitHandler}>
+                  <h1 className="text-2xl">Successfully scanned!</h1>
+                  <p className="text-lg">Please complete your password.</p>
+                  <div className="relative mb-6" data-te-input-wrapper-init>
+                    <input
+                      type="text"
+                      className="input input-bordered w-full max-w-xs"
+                      id="id"
+                      placeholder="id"
+                      defaultValue={uuid}
+                      hidden
+                    />
+                  </div>
+
+                  <div className="relative mb-6" data-te-input-wrapper-init>
+                    <input
+                      type="password"
+                      className="input input-bordered w-full max-w-xs"
+                      id="password"
+                      autoComplete="true"
+                      placeholder="Password"
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="text-center">
+                    <button
+                      type="submit"
+                      className="btn btn-success"
+                      data-te-ripple-init
+                      data-te-ripple-color="light"
+                    >
+                      Login
+                    </button>
+                  </div>
+                </form>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    );
   };
 
   return (
     <>
-      {!isValid && (
-        <div className="w-[300px] h-[450px] mx-auto mt-16 border shadow-xl rounded-3xl">
-          <div className="g-6 flex h-full flex-wrap items-center justify-center">
-            <div className="mb-12 md:mb-0 w-full text-center p-5">
-              {!isRead && (
-                <>
-                  {isReaderOn && (
-                    <QrReader
-                      onResult={async (result) => {
-                        if (result) {
-                          navigator.mediaDevices
-                            .getUserMedia({
-                              audio: false,
-                              video: true,
-                            })
-                            .then((stream) => {
-                              stream.getTracks().forEach(function (track) {
-                                track.stop();
-                                track.enabled = false;
-                              });
-                            });
-                          setIsRead(true);
-                          setUuid(result.getText());
-                        }
-                      }}
-                      constraints={{ facingMode: undefined }}
-                    />
-                  )}
-                  <button
-                    className="btn btn-primary mt-5"
-                    onClick={clickHandler}
-                  >
-                    Read QRCode
-                  </button>
-                </>
-              )}
-              {isRead && (
-                <>
-                  <form onSubmit={submitHandler}>
-                    <h1 className="text-2xl">Successfully scanned!</h1>
-                    <p className="text-lg">Please complete your password.</p>
-                    <div className="relative mb-6" data-te-input-wrapper-init>
-                      <input
-                        type="text"
-                        className="input input-bordered w-full max-w-xs"
-                        id="id"
-                        placeholder="id"
-                        defaultValue={uuid}
-                        hidden
-                      />
-                    </div>
-
-                    <div className="relative mb-6" data-te-input-wrapper-init>
-                      <input
-                        type="password"
-                        className="input input-bordered w-full max-w-xs"
-                        id="password"
-                        autoComplete="true"
-                        placeholder="Password"
-                        onChange={(e) => setPassword(e.target.value)}
-                      />
-                    </div>
-
-                    <div className="text-center">
-                      <button
-                        type="submit"
-                        className="btn btn-success"
-                        data-te-ripple-init
-                        data-te-ripple-color="light"
-                      >
-                        Login
-                      </button>
-                    </div>
-                  </form>
-                </>
-              )}
-            </div>
-          </div>
-        </div>
-      )}
-      {isValid && props.children}
+      {!isAuthenticated && <PasswordForm />}
+      {isAuthenticated && props.children}
     </>
   );
 };
